9b0a40e4a58ed6da92f9db29be98a39846990bff
n_nader <nadernadia@hotmail.com>
2026-03-26 09:06:20 -0300

    Empece con taylor, defini la funcion para todos los ejercicios y derivada

---
+++ TP2/Parte1/modules/funcion.py
@@ -0,0 +1,18 @@
+import sympy as sp
+
+def derivadas(func,orden):#Esto lo saque de Ecuaciones Diferenciales
+    for sym in func.free_symbols:
+        t = sym
+    derivadas = []
+    #derivada[0]=func
+    for i in range(orden):
+        derivadas.append(func.diff(t,i))
+    return derivadas
+
+# r=10.0
+# K=1000.0
+# p0=10.0
+#funcion P(t) = K/(1+(K/p0-1)*exp(-r*t)) ya esta resuelta
+t = sp.symbols('t', real=True)
+def P(t):
+    return 1000.0/(1+(1000.0/10.0-1)*sp.exp(-10.0*t))    
