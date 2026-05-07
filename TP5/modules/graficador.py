d4e598963217a948e7863a53877b3cd5787ab063
MariaPazA <mariapaz.almiron@ingenieria.uner.edu.ar>
2026-05-07 18:44:36 -0300

    subo cambios en graficas

--- TP5/modules/graficador.py
+++ TP5/modules/graficador.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 def graficar_resultados(datos, labels):
     '''Grafica i(t) y a(t)'''
     tiempo, solucion = datos
-    hs = solucion[:, 0]  # Humanos infectados
-    ms = solucion[:, 1]  # Mosquitos infectados
+    i = solucion[:, 0]  # Humanos infectados
+    a = solucion[:, 1]  # Mosquitos infectados
+    s = 1 - i           # humanos sanos (s = 1 - i)
+    v = 1 - a           # mosquitos sanos (v = 1 - a)
     
     fig, ax = plt.subplots(figsize=(9, 5))
 
-    ax.plot(tiempo, hs, label=f'Humanos infectados i(t)', color='steelblue')
-    ax.plot(tiempo, ms, label=f'Mosquitos infectados a(t)', color='deeppink')
+    ax.plot(tiempo, i, label=f'Humanos infectados i(t)', color='steelblue')
+    ax.plot(tiempo, a, label=f'Mosquitos infectados a(t)', color='deeppink')
+    ax.plot(tiempo, s, label=f'Humanos sanos s(t)', color='lightgreen')
+    ax.plot(tiempo, v, label=f'Mosquitos sanos v(t)', color='orange' )
 
     ax.set_ylim(0, 1) #tiene que ir de 0 a 1 porque son proporciones de la población total
     ax.set_xlabel('Tiempo (dias)')
@@ -27,11 +31,15 @@ def graficar_resultados_dist ( simulacion, labels):
     i = solucion[:, 0]  # Humanos infectados
     a = solucion[:, 1]  # Mosquitos infectados
     r = solucion[:, 2]  #Tercer compartimento (expuestos, recuperados o plasmidos)
+    s=1-i-r # humanos sanos 
+    v=1-a # mosquitos sanos 
 
     fig, ax = plt.subplots(figsize=(9, 5))
     ax.plot(tiempo, i, label=f'Humanos infectados i(t)', color='steelblue')
     ax.plot(tiempo, a, label=f'Mosquitos infectados a(t)', color='deeppink')
     ax.plot(tiempo, r, label=f'{labels[1]}', color='lightgreen')
+    ax.plot(tiempo, s, label=f'Humanos sanos s(t)', color='lightgray')
+    ax.plot(tiempo, v, label=f'Mosquitos sanos v(t)', color='orange')
 
     ax.set_ylim(0, 1) #tiene que ir de 0 a 1 porque son proporciones de la población total
     ax.set_xlabel('Tiempo (dias)')
@@ -51,9 +59,15 @@ def graficar_varias_ci(resultados, equilibrio):
     for ax, res in zip(axes.flat, resultados):
         t = res['tiempo']
         sol = res['solucion']
+        i = sol[:, 0]
+        a = sol[:, 1]
+        s = 1 - i   # humanos sanos
+        v = 1 - a   # mosquitos sanos
  
-        ax.plot(t, sol[:, 0], color='steelblue', label='i(t) humanos')
-        ax.plot(t, sol[:, 1], color='deeppink', label='a(t) mosquitos')
+        ax.plot(t, i, color='steelblue', label='i(t) humanos')
+        ax.plot(t, a, color='deeppink', label='a(t) mosquitos')
+        ax.plot(t, s, color='lightgreen', label='s(t) humanos')
+        ax.plot(t, v, color='orange', label='v(t) mosquitos')
 
         ax.set_title(res['label'], fontsize=10)
         ax.set_xlabel('Tiempo (días)')
