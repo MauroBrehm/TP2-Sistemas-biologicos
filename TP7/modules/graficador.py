4bd19c2571f8b3436b8df2c8d3033e12a65e3782
Mauro Brehm <maurobren@gmail.com>
2026-05-17 09:42:05 -0300

    Subo para empezar con el TP7

---
+++ TP7/modules/graficador.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+def graficar_resultados (simulacion):
+    '''Grafica modelo'''
+    tiempos = [p[0] for p in simulacion]
+    G6P = [p[1][0] for p in simulacion]
+    F6P = [p[1][1] for p in simulacion]
+    FBP = [p[1][2] for p in simulacion]
+    ATP = [p[1][3] for p in simulacion]
+    ADP = [p[1][4] for p in simulacion]
+    AMP = [p[1][5] for p in simulacion]
+
+    fig, ax = plt.subplots(figsize=(9, 5))
+    ax.plot(tiempos, G6P, label=f'G6P', color='steelblue')
+    ax.plot(tiempos, F6P, label=f'F6P', color='deeppink')
+    ax.plot(tiempos, FBP, label=f'FBP', color='lightgreen')
+    ax.plot(tiempos, ATP, label=f'ATP', color='purple')
+    ax.plot(tiempos, ADP, label=f'ADP', color='orange')
+    ax.plot(tiempos, AMP, label=f'AMP', color='cyan')
+
+    ax.set_ylim(0, 6)
+    ax.set_xlim(0, 1)
+    ax.set_xlabel('Tiempo (minutos)')
+    ax.set_ylabel('Concentración (mM)')   
+    ax.set_title(f'Modelo dinamico de glucolisis')
+
+    ax.legend()
+    ax.grid(True)
+    plt.tight_layout()
+    plt.show()
+
---
+++ TP7/modules/metodos.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# Método de Euler para resolver sistemas de EDOs
+def metod_euler(f , a, b, yi, h):
+    y = np.array(yi) #valor anterior
+    t = a  # tiempo inicial
+    m = int((b-a)/h)  # número de pasos
+    lista = [(t, y.copy())]  #se usa copy para guardar una copia del valor en cada paso, 
+                             #sino hacemos esto los valores se sobreescribirían y todas las graficas serian lineas
+    for i in range(m):
+        y = y + np.array(f(t, y)) * h
+        if np.any(y < 0):  # Evitar concentraciones negativas
+            y = np.maximum(y, 0)
+        t = t + h 
+        lista.append((t, y.copy()))  
+    return lista
