cda3bec8540af02920458c07ce3d3e5a4aa677c7
LuciaSaravia <luciaasaravia@gmail.com>
2026-05-20 21:02:25 -0300

    subo modificaciones

--- TP7/main.py
+++ TP7/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from modules.graficador import graficar_resultados
 
 #Parametros del modelo
 filas=50
@@ -16,12 +16,14 @@ G = 1.0
 #Umbrales para cambios de estado
 v_reposo = -90.0
 v_pico= 30.0
-v_prr = -0.001
+v_prr = 0.001
 
-#tasas de cambio 
-t_existacion =60.0 #mV/ms --> velocidad de subida en estado E
-t_repolariz=1.0 #mV/ms --> velocidad de bajada en estado PRR
-t_pra_exp =0.04 #fraccion por ms --> decaimiento exponencial en PRA (4% por ms)
+# Tasas de cambio
+t_exc = 60.0   # mV/ms --> velocidad de subida en estado excitado
+
+t_rep = 1.0    # mV/ms --> velocidad de bajada en PRR
+
+t_pra_exp = 0.04  # fracción de decaimiento en PRA por ms
 
 #Marcapasos --> frecuencia cardíaca de 60 latidos/minuto = 1 latido cada 1000 ms
 periodo_marcapasos = 1000.0 #ms
@@ -45,50 +47,77 @@ estado = np.full((filas, cols), reposo, dtype=int)
 #matriza de potenciales de membrana --> todas las celulas empiezan en potencial de reposo
 potencial = np.full((filas, cols), v_reposo, dtype=float) 
 
+
+def potencial_total_vertical(matriz):
+    """Calcula el potencial total vertical como suma de los dipolos verticales."""
+    dipolos = matriz[1:, :] - matriz[:-1, :]
+    return np.sum(dipolos)
+
 #registros de ECG a lo largo del tiempo
 ECG =[]
-
-nuevo_estado=estado.copy()
-nuevo_potencial=potencial.copy()
-for i in range(filas):
-    for j in range(cols):
-        V=potencial[i,j]
-        e=estado[i,j]
-        #Tema de los vecinos
-        vecinos = []
-        if i>0:
-            vecinos.append(potencial[i-1,j]) #vecino de arriba
-        if i<filas-1:
-            vecinos.append(potencial[i+1,j]) #vecino de abajo
-        if j>0:
-            vecinos.append(potencial[i,j-1]) #vecino de izquierda
-        if j<cols-1:
-            vecinos.append(potencial[i,j+1]) #vecino de derecha
-
-        #Corriente total recibida
-        I=G*sum(v-V for v in vecinos)
-        if estado[i,j]==reposo:
-            if I>=UR:
-                nuevo_estado[i,j]=excitado
-                nuevo_potencial[i,j]=v_pico
-        elif estado[i,j]==excitado:#Entra sodio por lo que el potencial sube rapido(ocurre despolarizacion)
-            nuevo_potencial[i,j]=min(v_pico, V+t_existacion*dt)
-            if nuevo_potencial[i,j]<=v_reposo:
-                nuevo_estado[i,j]=pra
-        elif estado[i,j]==pra:#No puede excitarse otra vez auneque el estimulo es fuerte(Si no, nos re morimos)
-            nuevo_potencial[i,j]=max(v_prr, V-t_pra_exp*V*dt)
-            if nuevo_potencial[i,j]<=v_prr:
-                nuevo_estado[i,j]=prr
-        elif estado[i,j]==prr:
-            nuevo_potencial[i,j]=max(v_reposo, V-t_repolariz*dt)
-            if nuevo_potencial[i,j]<=v_reposo:
-                nuevo_estado[i,j]=reposo
-estado=nuevo_estado.copy()
-potencial=nuevo_potencial.copy()
 #Esto es para el marcapaso
 for paso in range(n_pasos):
-    if paso%int(periodo_marcapasos/dt)==0:
-        potencial[0,:]=v_pico
-        estado[0,:]=excitado
+    if paso % int(periodo_marcapasos/dt) == 0:
+        potencial[0,:] = v_pico
+        estado[0,:] = excitado
+
+    nuevo_potencial = potencial.copy()
+    nuevo_estado = estado.copy()
+
+    for i in range(filas):
+        for j in range(cols):
+            V = potencial[i,j]
+            e = estado[i,j]
+
+            #Tema de los vecinos
+            vecinos = []
+            if i > 0:
+                vecinos.append(potencial[i-1,j]) #vecino de arriba
+            if i < filas-1:
+                vecinos.append(potencial[i+1,j]) #vecino de abajo
+            if j > 0:
+                vecinos.append(potencial[i,j-1]) #vecino de izquierda
+            if j < cols-1:
+                vecinos.append(potencial[i,j+1]) #vecino de derecha
+
+            
+            I = G*sum(v - V for v in vecinos) #Corriente total recibida
+            
+
+            # --- Reposo -> Excitado ---
+            if estado[i,j] == reposo and I >= UR:
+                nuevo_estado[i,j] = excitado
+                nuevo_potencial[i,j] = v_pico 
+
+            elif estado[i,j] == excitado:     #Entra sodio por lo que el potencial sube rapido(ocurre despolarizacion)
+                nuevo_potencial[i,j] = min(v_pico, V + t_exc * dt)
+                # --- Excitado -> PRA ---
+                if nuevo_potencial[i,j] >= v_pico: #cuando alcanza el pico, pasa a PRA
+                    nuevo_estado[i,j] = pra
+                    nuevo_potencial[i,j] = v_pico
+
+            elif estado[i,j] == pra:  #No puede excitarse otra vez auneque el estimulo es fuerte(Si no, nos re morimos)
+                nuevo_potencial[i,j] =  max(v_prr, V - t_pra_exp * V * dt)
+                # --- PRA -> PRR ---
+                if nuevo_potencial[i,j] <= v_prr: 
+                    nuevo_estado[i,j] = prr
+
+            elif estado[i,j] == prr:
+                nuevo_potencial[i,j] = max(v_reposo, V - t_rep * dt)
+
+                if nuevo_potencial[i,j] >= UP: #Si el estimulo es fuerte, puede volver a excitarse
+                    nuevo_estado[i,j] = excitado
+                    nuevo_potencial[i,j] = v_pico
+
+                if nuevo_potencial[i,j] <= v_reposo: 
+                    nuevo_estado[i,j] = reposo
+                    nuevo_potencial[i,j] = v_reposo
+                    
+            estado = nuevo_estado.copy()
+            potencial = nuevo_potencial.copy()
+            ECG.append(potencial_total_vertical(potencial))
+
+tiempos = np.arange(len(ECG)) * dt
+graficar_resultados(ECG, tiempos)
 
 
--- TP7/modules/graficador.py
+++ TP7/modules/graficador.py
@@ -1,31 +1,14 @@
 import matplotlib.pyplot as plt
 
-def graficar_resultados (simulacion):
+def graficar_resultados (simulacion, tiempos):
     '''Grafica modelo'''
-    tiempos = [p[0] for p in simulacion]
-    G6P = [p[1][0] for p in simulacion]
-    F6P = [p[1][1] for p in simulacion]
-    FBP = [p[1][2] for p in simulacion]
-    ATP = [p[1][3] for p in simulacion]
-    ADP = [p[1][4] for p in simulacion]
-    AMP = [p[1][5] for p in simulacion]
-
-    fig, ax = plt.subplots(figsize=(9, 5))
-    ax.plot(tiempos, G6P, label=f'G6P', color='steelblue')
-    ax.plot(tiempos, F6P, label=f'F6P', color='deeppink')
-    ax.plot(tiempos, FBP, label=f'FBP', color='lightgreen')
-    ax.plot(tiempos, ATP, label=f'ATP', color='purple')
-    ax.plot(tiempos, ADP, label=f'ADP', color='orange')
-    ax.plot(tiempos, AMP, label=f'AMP', color='cyan')
-
-    ax.set_ylim(0, 6)
-    ax.set_xlim(0, 1)
-    ax.set_xlabel('Tiempo (minutos)')
-    ax.set_ylabel('Concentración (mM)')   
-    ax.set_title(f'Modelo dinamico de glucolisis')
-
-    ax.legend()
-    ax.grid(True)
+    plt.figure(figsize=(10, 4))
+    plt.plot(tiempos, simulacion, color='pink', label='ECG Simulado')
+    plt.title('Evolución temporal del potencial cardíaco (derivación bipolar II)')
+    plt.xlabel('Tiempo (ms)')
+    plt.ylabel('Potencial total vertical')
+    plt.grid(True)
     plt.tight_layout()
     plt.show()
 
+  
