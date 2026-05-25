f1e6397cda646ff1453b218f75d5ebc055885d8c
Mauro Brehm <maurobren@gmail.com>
2026-05-25 17:37:45 -0300

    Ya esta para empezar con el tp8

---
+++ TP8/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.colors import ListedColormap
+
+from modules.graficador import graficar_resultados
+
+
